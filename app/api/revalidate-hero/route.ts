d640dfd86a7d33bc44888929e00737c2942dd1b7
Yushan Fernando <yushanwebdev@gmail.com>
2024-08-31 17:09:52 +0530

    main: Update the revalidate hero API

--- app/api/revalidate-hero/route.ts
+++ app/api/revalidate-hero/route.ts
@@ -1,5 +1,8 @@
 import { revalidateTag } from "next/cache";
+import { NextResponse } from "next/server";
 
 export async function GET() {
   revalidateTag("hero");
+
+  return NextResponse.json({ revalidated: true });
 }
